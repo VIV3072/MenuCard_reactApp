e67db019e8f6c0feb0be3b7aa015c67db596c6d8
vivek singh <103558318+VIV3072@users.noreply.github.com>
2023-02-16 16:04:37 +0530

    first_react_app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import resturent from './component/resturent';
+
+
+const App = () => {
+  return (
+    <>    
+      <resturent/>
+    
+    </>
+  )
+}
+
+export default resturent
+
+
